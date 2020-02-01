eb6891aa31104072515fee5cb5d3e625803bf211
colemalphrus <cole@malphrusfamilymission.com>
2020-02-01 15:34:54 -0500

    added readme and  license

---
+++ README.md
@@ -0,0 +1,16 @@
+# timeconMT
+A simple time conversion by Malphrus Tech
+
+## Usage
+
+### import
+```python
+from csv2dictionary.csvpro import CsvBrain
+```
+### use
+```python
+case1 = CsvBrain('test.csv')
+case1.dict
+#returns a list of dictionaries
+#the first line in the csv are the keys in the dictionaries
+```
---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="csv2dictionary",
+    version="0.0.1",
+    author="Cole Malphrus",
+    author_email="cole@malphrus.tech",
+    description="converts csv to a dictionary",
+    long_description=long_description,
+    url="https://github.com/colemalphrus/csv2dictionary",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
